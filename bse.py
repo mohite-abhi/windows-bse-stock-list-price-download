18626468ebd4dd9be031e8e03355f102abde5f64
mohite-abhi <b518004@iiit-bh.ac.in>
2021-01-08 12:37:58 +0530

    add numbering

--- bse.py
+++ bse.py
@@ -1,7 +1,7 @@
 import sys
 import xlsxwriter
 from bsedata.bse import BSE
-from time import sleep
+from time import sleep, time
 b = BSE()
 
 try:
@@ -16,59 +16,90 @@ except:
     sleep(15)
     sys.exit()
 
-quotes = []
+set1 = set()
+for i in codelist:
+    set1.add(i)
+codelist = set1
 
-print("\ninternet se connect rahe aur kuch second wait kare")
-print("bse-quotation-number file me die gae stock ka price download ho raha hai")
-print("please wait...")
-sleep(1)
-
-try:
-    b.getQuote("532978")
-except:
-    print("\nplease connect internet to download stock price")
-    sleep(15)
-    sys.exit()
+def waitFor(codelist):
     
-i = 1
-for code in codelist:
+    
+    waittime = 0
+
     try:
-        quotes.append(b.getQuote(code))
+        start = time()
+        b.getQuote("532978")
+        end = time()
+        waittime = end - start
     except:
-        print("\nbse-quotation-number file me quotation ",code," me problem hai jo line ",i," me likha hai")
-        print("please wait...")        
-    i+=1
-quotes = sorted(quotes, key = lambda i: i['pChange'], reverse=True)
+        print("\n----------Please connect internet !----------")
+        sleep(15)
+        sys.exit()
 
-kk = [["companyName",'currentValue', 'change', '% Change', 'time']] + [[i["companyName"], i['currentValue'], float(i['change']), float(i['pChange']), i['updatedOn'][12:17] ]for i in quotes]
 
-with xlsxwriter.Workbook('stock-today.xlsx') as workbook:
-    worksheet = workbook.add_worksheet()
-    red = workbook.add_format()
-    red.set_font_color('red')
-    green = workbook.add_format()
-    green.set_font_color('green')
-    font_color = workbook.add_format()
+    waittime *= len(codelist)
+    waittime = int(waittime)
+    waittime = [waittime//60, waittime%60]
+
+    print("\n----------Keep stock-today excel file closed----------")
+    print("----------Stay connected to internet and wait "+str(waittime[0])+" min "+str(waittime[1])+" sec----------")
+  
+def mainProgram(codelist):
+    quotes = []
+
+
+    i = 1
+    for code in codelist:
+        try:
+            quotes.append(b.getQuote(code))
+        except:
+            print("\nbse-quotation-number file me quotation ", code,
+                  " me problem hai jo line ", i, " me likha hai")
+            print("please wait...")
+        i += 1
+    quotes = sorted(quotes, key=lambda i: float(i['pChange']), reverse=True)
+
+    kk = [["No","companyName", 'currVal', '    dayHigh', '    dayLow', '    % Change', '      change',  'time']] + [[i["companyName"], i['currentValue'],
+                                            float(i['dayHigh']), float(i['dayLow']), float(i['pChange']), float(i['change']), i['updatedOn'][12:17]]for i in quotes]
+
+
+    for i in range(1,len(kk)):
+        kk[i] = [i]+kk[i]
+
+    with xlsxwriter.Workbook('stock-today.xlsx') as workbook:
+        worksheet = workbook.add_worksheet()
+        red = workbook.add_format()
+        red.set_font_color('red')
+        green = workbook.add_format()
+        green.set_font_color('green')
+        font_color = workbook.add_format()
+
+        for row_num, data in enumerate(kk):
+            worksheet.write_row(row_num, 0, data)
+
+        ln = str(len(kk))
+        worksheet.conditional_format('F2:G'+ln, {
+            'type': 'cell',
+            'criteria': '>=',
+            'value': 0,
+            'format': green})
+
+        worksheet.conditional_format('F2:G'+ln, {
+            'type': 'cell',
+            'criteria': '<',
+            'value': 0,
+            'format': red})
+
+        worksheet.set_column('A:A', 2, None)
+        worksheet.set_column('B:B', 28, None)
+        worksheet.set_column('C:G', 11, None)
+
+    print("\n----------Done !----------")
+    sleep(20)
+    sys.exit()
+
+
+waitFor(codelist)
+mainProgram(codelist)
+
     
-    for row_num, data in enumerate(kk):
-        worksheet.write_row(row_num, 0, data)
-
-    ln = str(len(kk))
-    worksheet.conditional_format('C2:D'+ln, {
-            'type': 'cell',
-            'criteria' : '>=',
-            'value':0,
-            'format':green})
-
-    worksheet.conditional_format('C2:D'+ln, {
-            'type': 'cell',
-            'criteria' : '<',
-            'value':0,
-            'format':red})
-
-    worksheet.set_column('A:A', 55, None)
-
-print("\nAaj ke stock price stock-today excel file me ja chuke hain, please use copy kar le")
-print("Next time program chalane par us file ka data delete ho kar naya data aa jaega")
-sleep(20)
-sys.exit()
